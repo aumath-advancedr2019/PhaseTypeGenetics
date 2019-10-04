e2a18cfb5d48649f656309bdaf4e62e92e95b534
JetteS <jette.steinbach@post.au.dk>
2019-10-04 13:17:53 +0200

    The required documents and functions for calculating the mean and variance

---
+++ MeanAndVariance.R
@@ -0,0 +1,23 @@
+
+mean.phasetype <- function(initDist, Tmat){
+  
+  return(LaplacePhaseType(initDist, Tmat, i=1))
+  
+}
+
+var <- function(...){
+  
+  UseMethod("var")
+}
+
+var.default <- function(x,...){
+  
+  var(x,...)
+}
+
+var.phasetype <- function(){
+  
+  return(LaplacePhaseType(initDist = q, Tmat = TmatMRCA, i=2)-
+    LaplacePhaseType(initDist = q, Tmat = TmatMRCA, i=1)^2)
+}
+

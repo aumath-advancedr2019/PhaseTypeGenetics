da794d66060dab1a8982d8b7046c1c8699be8759
PeterHTKrarup <54469310+PeterHTKrarup@users.noreply.github.com>
2019-10-06 22:26:16 +0200

    Update Constructors&Summaries.R
    Ændrede navnet på dphasetype klassen så det i stedet kan bruges til tæthedsfunktionen.

--- Constructors&Summaries.R
+++ Constructors&Summaries.R
@@ -1,27 +1,27 @@
 # Constructor for continuous phase-type distribution
-cphasetype <- function(initDist, T.mat){
+contphasetype <- function(initDist, T.mat){
   if((sum(initDist) > 1) | (sum(initDist < 0) > 0)) stop("Not a valid initial distribution")
   if(det(T.mat)==0) stop("Not a valid subintensity matrix")
   if(length(initDist) != nrow(T.mat)) stop("The dimensions of the input should be the same")
   tmp <- list("initDist" = initDist, "T.mat" = T.mat)
-  class(tmp) <- "cphasetype"
+  class(tmp) <- "contphasetype"
   tmp
 }
 
 # Constructor for discrete phase-type distribution
-dphasetype <- function(initDist, T.mat){
+discphasetype <- function(initDist, T.mat){
   if((sum(initDist) > 1) | (sum(initDist < 0) > 0)) stop("Not a valid initial distribution")
   if((det(T.mat) == 0) | (sum(T.mat < 0) == 0) | (sum(rowSums(T.mat) > 1) > 0)) {
     stop("Not a valid subtransition matrix") 
   }
   if(length(initDist) != nrow(T.mat)) stop("The dimensions of the input should be the same")
   tmp <- list("initDist" = initDist, "T.mat" = T.mat)
-  class(tmp) <- "dphasetype"
+  class(tmp) <- "discphasetype"
   tmp
 }
 
 # Summary for continuous phase-type distribution
-summary.cphasetype <- function(x){
+summary.contphasetype <- function(x){
   cat("A continuous phase-type distribution with initial probability vector \n")
   print(x$initDist)
   cat("and subintensity matrix \n")
@@ -32,7 +32,7 @@ summary.cphasetype <- function(x){
 }
 
 # Summary for discrete phase-type distribution
-summary.dphasetype <- function(x){
+summary.discphasetype <- function(x){
   cat("A discrete phase-type distribution with initial probability vector \n")
   print(x$initDist)
   cat("and subtransition matrix \n")

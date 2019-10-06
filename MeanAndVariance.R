03a51e150052242dfa924b9a68f795f4c68d9a7f
JetteS <jette.steinbach@post.au.dk>
2019-10-06 10:26:57 +0200

    An updated version of the mean and variance for discrete and continuous phase-type distributions

--- MeanAndVariance.R
+++ MeanAndVariance.R
@@ -1,23 +1,98 @@
+##----------------------------------------------------
+## The mean of a continuous phase-type distribution
+##----------------------------------------------------
+## Name: mean.cphasetype
+## Purpose: Computing the mean of a continuous phase-
+##          type distribution (Corollary 1.2.64 in [BN])
+## Input:
+## T.mat = the subintensity matrix from the
+##        continuous phase type distribution
+## initDist = the initial distribution
+## Output: 
+## The mean E[tau] 
+##----------------------------------------------------
 
-mean.cphasetype <- function(initDist, Tmat){
+mean.cphasetype <- function(initDist, T.mat){
   
-  return(LaplacePhaseType(initDist, Tmat, i=1))
+  e.vec <- replicate(n=ncol(T.mat),1)
+  return(initDist%*%solve(-T.mat)%*%e.vec)
   
 }
+
+##----------------------------------------------------
+## The mean of a discrete phase-type distribution
+##----------------------------------------------------
+## Name: mean.dphasetype
+## Purpose: Computing the mean of a discrete phase-
+##          type distribution (Corollary 1.2.64 in [BN])
+## Input:
+## T.mat = the subintensity matrix from the
+##        discrete phase type distribution
+## pi.vec = the initial distribution
+## Output: 
+## The mean E[tau] 
+##----------------------------------------------------
+
+mean.dphasetype <- function(T.mat, pi.vec){
+  
+  e.vec <- replicate(n=ncol(T.mat),1)
+  return(pi.vec%*%solve(diag(x=1, nrow = nrow(T.mat))-T.mat)%*%e.vec)
+  
+}
+
+## Defining a generic var() function
 
 var <- function(...){
   
   UseMethod("var")
 }
 
+## The default 
 var.default <- function(x,...){
   
   var(x,...)
 }
 
-var.cphasetype <- function(){
+
+##----------------------------------------------------
+## The variance of a discrete phase-type distribution
+##----------------------------------------------------
+## Name: var.dphasetype
+## Purpose: Computing the variance of a discrete phase-
+##          type distribution 
+##          (using Theorem 1.2.69 in [BN])
+## Input:
+## T.mat = the subintensity matrix from the
+##        discrete phase type distribution
+## pi.vec = the initial distribution
+## Output: 
+## The variance Var[tau] 
+##----------------------------------------------------
+var.dphasetype <- function(T.mat,pi.vec){
   
-  return(LaplacePhaseType(initDist = q, Tmat = TmatMRCA, i=2)-
-    LaplacePhaseType(initDist = q, Tmat = TmatMRCA, i=1)^2)
+  e.vec <- replicate(n=ncol(T.mat),1)
+  secondMoment <- 2*pi.vec%*%T.mat%*%solve((diag(x=1, nrow = nrow(T.mat))-T.mat)%^%2)%*%e.vec
+  firstmoment <- pi.vec%*%solve(diag(x=1, nrow = nrow(T.mat))-T.mat)%*%e.vec
+  return(secondMoment +firstmoment - firstmoment^2)
+}
+
+##----------------------------------------------------
+## The variance of a continuous phase-type distribution
+##----------------------------------------------------
+## Name: var.cphasetype
+## Purpose: Computing the variance of a continuous 
+##          phase-type distribution 
+##          (using Theorem 1.2.69 in [BN])
+## Input:
+## T.mat = the subintensity matrix from the
+##        discrete phase type distribution
+## initDist = the initial distribution
+## Output: 
+## The variance Var[tau] 
+##----------------------------------------------------
+var.cphasetype <- function(T.mat, initDist){
+  
+  return(LaplacePhaseType(initDist = initDist, Tmat = T.mat, i=2)-
+    LaplacePhaseType(initDist = initDist, Tmat = T.mat, i=1)^2)
 }
 

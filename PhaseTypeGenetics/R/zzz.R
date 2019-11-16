c96e867fdbc7ba9ec451f4b169cbe105fb599cd0
JetteS <jette.steinbach@post.au.dk>
2019-11-16 17:01:42 +0100

    Changing .onLoad to .onAttach and changing startup message

--- PhaseTypeGenetics/R/zzz.R
+++ PhaseTypeGenetics/R/zzz.R
@@ -1,35 +1,11 @@
-.onLoad <- function(libname=NULL, pkgname){
+.onAttach <- function(libname, pkgname= "PhaseTypeGenetics"){
 
-  expmpackage <- nzchar(system.file(package = "expm"))
-  partitionspackage <- nzchar(system.file(package = "partitions"))
+  packageStartupMessage(" Thank you for choosing the package *PhaseTypeGenetics*. \n
+                        We appreciate your support!\n
+                        \n
+                        Report a bug or ask a question about PhaseTypeGenetics at \n
+                        https://github.com/aumath-advancedr2019/phastypdist/issues.\n
+                        \n
+                        Now let's get started!")
 
-  if(expmpackage & partitionspackage){
-
-    cat("This package requires the packages *expm* and *partitions*.\n
-Both packages are installed on your device and will now be loaded...\n")
-
-    library("expm")
-
-  }else if(expmpackage & partitionspackage==FALSE){
-
-    cat("This package requires the packages *expm* and *partitions*. \n
-It seems that the package *partitions* is not installed on your device.\n
-You need to install this package in order to be able to use all functions.\n
-The package *exmp* is installed on your device and will now be loaded...\n")
-
-    library("expm")
-
-  }else if(expmpackage==FALSE & partitionspackage){
-
-    cat("This package requires the packages *expm* and *partitions*.\n
-It seems that the package *expm* is not installed on your device.\n
-You need to install this package in order to be able to use all functions.\n")
-
-  }else{
-
-    cat("This package requires the packages *expm* and *partitions*.\n
-It seems that none of the packages are installed on your device.\n
-Please install both packages in order to be able to use all
-functions.\n")
-  }
 }

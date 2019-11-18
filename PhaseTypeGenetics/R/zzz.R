6efdbb65b22ae6194db1442b65808d3895f71ccf
JetteS <jette.steinbach@post.au.dk>
2019-11-18 13:56:02 +0100

    Small change to the .onAttach file

--- PhaseTypeGenetics/R/zzz.R
+++ PhaseTypeGenetics/R/zzz.R
@@ -1,11 +1,11 @@
 .onAttach <- function(libname, pkgname= "PhaseTypeGenetics"){
 
-  packageStartupMessage(" Thank you for choosing the package *PhaseTypeGenetics*. \n
-                        We appreciate your support!\n
-                        \n
-                        Report a bug or ask a question about PhaseTypeGenetics at \n
-                        https://github.com/aumath-advancedr2019/phastypdist/issues.\n
-                        \n
-                        Now let's get started!")
+  packageStartupMessage(" Thank you for choosing the package 'PhaseTypeGenetics'. \n
+We appreciate your support!\n
+\n
+Report a bug or ask a question about PhaseTypeGenetics at \n
+https://github.com/aumath-advancedr2019/phastypdist/issues.\n
+\n
+Now let's get started!")
 
 }

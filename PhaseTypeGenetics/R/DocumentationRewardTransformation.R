6b69364a9dc2295e7fbabd5d975898cad7d3834d
JetteS <jette.steinbach@post.au.dk>
2019-11-18 12:55:50 +0100

    Changing T.mat to T_Mat and small changes

--- PhaseTypeGenetics/R/DocumentationRewardTransformation.R
+++ PhaseTypeGenetics/R/DocumentationRewardTransformation.R
@@ -1,19 +1,19 @@
 #' Reward transformation
 #'
-#' Reward transforming a continuous phase-type disctribution with initial distribution
-#' \code{initDist} and sub-intensity rate matrix \code{T.mat}.
+#' Reward transforming a continuous phase-type distribution with initial distribution
+#' \code{initDist} and sub-intensity rate matrix \code{T_Mat}.
 #'
-#' It is possible to assign a linear (nonnegative) reward to each of the transient states
+#' It is possible to assign a linear (non-negative) reward to each of the transient states
 #' of the Markov jump process underlying the phase-type distribution under consideration.
 #' More precisely, a reward is earned in each transient state and that reward is
 #' proportional to the time spent in the state. By assigning rewards, the original
 #' phase-type distribution is transformed into another continuous phase-type distribution.
-#' The details of the reward transformation can be found in Mogens Bladt and Bo Friis Nielsen (2017):
+#' More details on the reward transformation can be found in \code{vignette("PhaseTypeGenetics")} or in Mogens Bladt and Bo Friis Nielsen (2017):
 #' \emph{ Matrix-Exponential Distributions in Applied Probability}.
 #'
 #' @param object a continuous phase-type distributed object of class \code{contphasetype}.
 #' @param rewards a non-negative reward vector. The length of the reward vector
-#' should be equal to the number of rows of the subintensity rate matrix \code{T.mat}.
+#' should be equal to the number of rows of the sub-intensity rate matrix \code{T_Mat}.
 #'
 #' @return The function returns the reward transformed phase-type distribution,
 #' which is again an object of type \code{contphasetype}.
@@ -28,15 +28,15 @@
 #' ## The time to the most recent common ancestor (T_MRCA)
 #' ## is phase-type distributed with initial distribution
 #' initDist <- c(1,0,0,0)
-#' ## and subintensity rate matrix
-#' Tmat <- matrix(c(-6,6,0,0,
+#' ## and sub-intensity rate matrix
+#' T_Mat <- matrix(c(-6,6,0,0,
 #'                   0,-3,2,1,
 #'                   0,0,-1,0,
 #'                   0,0,0,-1), nrow = 4, byrow = TRUE)
 #' ## Defining an object of type "contphasetype"
-#' TMRCA <- contphasetype(initDist, Tmat)
+#' TMRCA <- contphasetype(initDist, T_Mat)
 #' ## In order to obtain the distribution of the total
-#' ## length of all branches giving rise to singeltons,
+#' ## length of all branches giving rise to singletons,
 #' ## we have to give the following rewards to the
 #' ## different states
 #' r.vec <- c(4,2,1,0)
@@ -49,7 +49,7 @@ RewTransDistribution <- function(object, rewards){
 
   if(class(object) != "contphasetype") stop("Invalid object! The object has to be of type 'contphasetype'.")
 
-  if(sum(rewards < 0) > 0 | sum(rewards)==0 | length(rewards)!=nrow(object$T.mat)){
+  if(sum(rewards < 0) > 0 | sum(rewards)==0 | length(rewards)!=nrow(object$T_Mat)){
 
     stop("The reward vector has to be nonnegative and contain at least one positve number.\n
          Furthermore, the length of the reward vector has to be equal to the number of rows of the sub-intensity rate matrix.")
@@ -58,13 +58,13 @@ RewTransDistribution <- function(object, rewards){
 
     #We extract the initial distribution and subintensity matrix from the object
     initDist <- object$initDist
-    T.mat <- object$T.mat
+    T_Mat <- object$T_Mat
 
     ## We define the sets S^+ and S^0
     S.plus <- which(rewards>0)
     S.zero <- which(rewards==0)
     ## as well as the matrix Q
-    Q.mat <- -T.mat/diag(T.mat)
+    Q.mat <- -T_Mat/diag(T_Mat)
     diag(Q.mat) <- 0
 
     ## We rearrange the states according to the
@@ -112,20 +112,20 @@ RewTransDistribution <- function(object, rewards){
     }
 
     ## Now we can define the subintensity matrix
-    ## newT.mat as
+    ## newT_Mat as
     if(length(S.plus)==1){
 
-      newT.mat <- -T.mat[S.plus,S.plus]/rewards[S.plus]*P.mat
-      exitrate <- -T.mat[S.plus,S.plus]/rewards[S.plus]*p.vec
+      newT_Mat <- -T_Mat[S.plus,S.plus]/rewards[S.plus]*P.mat
+      exitrate <- -T_Mat[S.plus,S.plus]/rewards[S.plus]*p.vec
     }else{
 
-      newT.mat <- -diag(T.mat[S.plus,S.plus])/rewards[S.plus]*P.mat
-      exitrate <- -diag(T.mat[S.plus,S.plus])/rewards[S.plus]*p.vec
+      newT_Mat <- -diag(T_Mat[S.plus,S.plus])/rewards[S.plus]*P.mat
+      exitrate <- -diag(T_Mat[S.plus,S.plus])/rewards[S.plus]*p.vec
     }
 
 
-    diag(newT.mat) <- -rowSums(newT.mat)-exitrate
+    diag(newT_Mat) <- -rowSums(newT_Mat)-exitrate
 
   }
-  return(contphasetype(initDist = newInitDist, T.mat = newT.mat))
+  return(contphasetype(initDist = newInitDist, T_Mat = newT_Mat))
 }
